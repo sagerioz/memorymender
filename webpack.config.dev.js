29ce85d20a0562550e8f3db4c35517fa5d23f6a5
sagerioz <ericaepperson27@yahoo.com>
2017-05-28 07:14:17 -0600

    first component built and webpack working

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,38 @@
 import path from 'path'
+import webpack from 'webpack';
 
 export default {
   devtools: 'eval-source-map',
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
   output: {
-    path: '/'
+    path: '/',
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         include: path.join(__dirname, 'client'),
-        loaders: [ 'babel' ]
+        loaders: 'babel',
+        query: {
+          presets: [ 'react' ]
+        }
       }
     ]
   },
+  entry: [
+  'webpack-hot-middleware/client?reload=true',
+  path.join(__dirname, './client/index.js'),
+ ],
   resolve: {
     extentions: [ '', '.js' ]
   }
